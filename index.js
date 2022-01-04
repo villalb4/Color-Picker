15b1ae6737966dd410819589f05a71a9cb44b9e5
villalb4 <villalb4angel@gmail.com>
2022-01-04 18:16:10 -0300

    Update

---
+++ index.js
@@ -0,0 +1,7 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025" ];
+const button = document.getElementById("button");
+const color = document.querySelector(".color")
+
+button.addEventListener('click' ,function() {
+  console.log(document.body)
+})
