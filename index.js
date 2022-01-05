6de22fe98e4e856618fae7347e421b9830aa31d9
Angel Villalba <79768764+villalb4@users.noreply.github.com>
2022-01-05 14:52:35 -0300

    Update Project

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025" ];
+const colors = ["maroon", "red", "orange", "yellow", "olive", "purple", "fuchsia", "white", "lime", "green", "navy", "blue", "aqua", "teal", "black", "silver", "gray"];
 const button = document.getElementById("button");
 const color = document.querySelector(".color")
 
-button.addEventListener('click' ,function() {
-  console.log(document.body)
-})
+button.addEventListener("click", function() {
+  const randomNumber = getRandomNumber();
+  console.log(randomNumber);
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
